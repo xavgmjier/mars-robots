887aace3feb3390f3eee81bd8937b7778924fcb6
Daniel <test@tests-MBP.powerhub>
2025-07-31 19:40:42 +0100

    First test for left turn

--- index.test.ts
+++ index.test.ts
@@ -1,3 +1,25 @@
-test('test 1', () => {
-    expect(true).toBe(true)
-})
+import { testFunc } from "./index.ts"
+import { describe, expect, it, test } from 'vitest'
+
+type Orientation = "N" | "E" | "S" | "W"
+
+const turnLeft = (x: Orientation) => {
+    const turnLeftMapping: Record<Orientation, Orientation> = {
+        "N" : "W",
+        "W" : "S",
+        "S" : "E",
+        "E" : "N",
+    }
+    return turnLeftMapping[x]
+}
+
+test.each`
+    original | expected
+    ${"N"}   ${"W"} 
+    ${"W"}   ${"S"} 
+    ${"S"}   ${"E"} 
+    ${"E"}   ${"N"} 
+    ` ("When facing $original, turning LEFT will result in facing $expected",
+    ({ original, expected }) => expect(turnLeft(original)).toBe(expected)
+)
+  
