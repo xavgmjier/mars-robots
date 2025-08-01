0ce9670e45a5de9d2aeff10c2c175f20faa42e8c
Daniel <test@tests-MBP.powerhub>
2025-08-01 18:25:50 +0100

    Added first logic and tests for executing turn and movement commands

--- index.test.ts
+++ index.test.ts
@@ -1,25 +1,135 @@
-import { testFunc } from "./index.ts"
+import { execute, MarsCoordinates, Orientation, Robot } from "./index.ts"
 import { describe, expect, it, test } from 'vitest'
 
-type Orientation = "N" | "E" | "S" | "W"
 
-const turnLeft = (x: Orientation) => {
-    const turnLeftMapping: Record<Orientation, Orientation> = {
-        "N" : "W",
-        "W" : "S",
-        "S" : "E",
-        "E" : "N",
-    }
-    return turnLeftMapping[x]
-}
+const START_POSITION: MarsCoordinates = [1, 1]
+
+const robot = (orientation: Orientation, position?: MarsCoordinates ): Robot => ({
+    orientation,
+    position: position || START_POSITION
+}) 
+
+// test.each`
+//     original | expected
+//     ${"N"}   ${"W"} 
+//     ${"W"}   ${"S"} 
+//     ${"S"}   ${"E"} 
+//     ${"E"}   ${"N"} 
+//     ` ("When facing $original, turning LEFT will result in facing $expected",
+//     ({ original, expected }) => expect(turnLeft(original)).toBe(expected)
+// )
 
 test.each`
-    original | expected
-    ${"N"}   ${"W"} 
-    ${"W"}   ${"S"} 
-    ${"S"}   ${"E"} 
-    ${"E"}   ${"N"} 
-    ` ("When facing $original, turning LEFT will result in facing $expected",
-    ({ original, expected }) => expect(turnLeft(original)).toBe(expected)
+    original | expected | direction
+    ${"N"}   ${"W"}     ${"L"}
+    ${"W"}   ${"S"}     ${"L"}
+    ${"S"}   ${"E"}     ${"L"}
+    ${"E"}   ${"N"}     ${"L"}
+    ${"N"}   ${"E"}     ${"R"}
+    ${"W"}   ${"N"}     ${"R"}
+    ${"S"}   ${"W"}     ${"R"}
+    ${"E"}   ${"S"}     ${"R"}
+
+    ` ("(execute) When facing $original, turning $direction will result in facing $expected",
+    ({ original, expected, direction }) => {
+        // const initialState: Rover = { orientation: original, position: [1, 1] }
+        expect(execute(direction, robot(original))).toEqual(robot(expected))
+    }
 )
-  
+
+// test.each`
+//     original | expected
+//     ${"N"}   ${"E"} 
+//     ${"W"}   ${"N"} 
+//     ${"S"}   ${"W"} 
+//     ${"E"}   ${"S"} 
+//     ` ("(execute) When facing $original, turning RIGHT will result in facing $expected",
+//    ({ original, expected }) => {
+//         const initialState: Rover = {orientation: original, position: [1, 1]}
+
+//         expect(execute("R", initialState)).toEqual({...initialState, orientation: expected})
+//     }
+// )
+
+// test.each`
+//     original | expected
+//     ${"N"}   ${"E"} 
+//     ${"W"}   ${"N"} 
+//     ${"S"}   ${"W"} 
+//     ${"E"}   ${"S"} 
+//     ` ("When facing $original, turning RIGHT will result in facing $expected",
+//     ({ original, expected }) => expect(turnRight(original)).toBe(expected)
+// )
+
+
+test("(execute) When moving north, we increment the Y Coordinate", () => {
+    // const initialState: Rover = { orientation: "N", position: [1, 1] }
+    expect(execute("F", robot("N"))).toEqual(robot("N", [1, 2]))
+
+})
+
+// test.each`
+//     orientation|initialPosition|expectedPosition|operation|axis
+//     ${"N"}      ${[1, 1]}      ${[1, 2]}        increment  Y
+//     ${"E"}      ${[1, 1]}      ${[2, 1]}        increment  X
+//     ${"S"}      ${[1, 1]}      ${[1, 0]}        decrement  Y
+//     ${"W"}      ${[1, 1]}      ${[0, 1]}        decrement  X
+
+//     ` ("(execute) When moving $orientation, we $operation the $axis coordinate",
+//     ({ orientation, initialPosition, expectedPosition }) => {
+//         // const initialState: Rover = { orientation, position: initialPosition }
+//         // const position = expectedPosition as 
+//         expect(execute("F", { orientation, position: initialPosition })).toEqual({
+//             position: expectedPosition
+//         })
+//     }
+// )
+
+test("(execute) When moving east, we increment the X Coordinate", () => {
+    // const initialState: Rover = { orientation: "E", position: [1, 1] }
+    expect(execute("F", robot("E"))).toEqual(robot("E", [2, 1]))
+
+})
+
+test("(execute) When moving south, we decrement the Y Coordinate", () => {
+    // const initialState: Rover = { orientation: "S", position: [1, 1] }
+    expect(execute("F", robot("S"))).toEqual(robot("S", [1, 0]))
+
+})
+
+test("(execute) When moving west, we decrement the X Coordinate", () => {
+    // const initialState: Rover = { orientation: "W", position: [1, 1] }
+    expect(execute("F", robot("W"))).toEqual(robot("W", [0, 1]))
+
+})
+
+
+ /////////////
+
+
+test("(execute) When executing a string if commands", () => {
+    // const initialState: Rover = { orientation: "W", position: [1, 1] }
+    expect(execute("LFLFLFLFF", robot("N", [1,2]))).toEqual(robot("N", [1, 3]))
+
+})
+
+
+// test("When moving north, we increment the Y Coordinate", () => {
+//     expect(move("N", [1, 1])).toEqual([1, 2])
+
+// })
+
+// test("When moving east, we increment the X Coordinate", () => {
+//     expect(move("E", [1, 1])).toEqual([2, 1])
+
+// })
+
+// test("When moving south, we decrement the Y Coordinate", () => {
+//     expect(move("S", [1, 1])).toEqual([1, 0])
+
+// })
+
+// test("When moving west, we decrement the X Coordinate", () => {
+//     expect(move("W", [1, 1])).toEqual([0, 1])
+
+// })
--- index.ts
+++ index.ts
@@ -1,1 +1,70 @@
-export const testFunc = () => true
+export type Orientation = "N" | "E" | "S" | "W"
+export type MarsCoordinates = [x: number, y: number]
+
+export type Robot = {
+    orientation: Orientation,
+    position: MarsCoordinates
+}
+
+const turnLeft = (x: Orientation) => (robot: Robot): Robot => {
+    // find a way to consolidate turnLeft() and turnRight()
+    const turnLeftMapping: Record<Orientation, Orientation> = {
+        "N": "W",
+        "W": "S",
+        "S": "E",
+        "E": "N",
+    }
+    const updatedOrientation = turnLeftMapping[x]
+    return {...robot, orientation: updatedOrientation}
+}
+
+const turnRight = (x: Orientation) => (robot: Robot): Robot => {
+    const turnRightMapping: Record<Orientation, Orientation> = {
+        "N": "E",
+        "E": "S",
+        "S": "W",
+        "W": "N",
+    }
+    const updatedOrientation = turnRightMapping[x]
+    return {...robot, orientation: updatedOrientation}
+
+}
+
+const getNextPos = (orientation: Orientation, position: MarsCoordinates): MarsCoordinates => {
+    // const {orientation, position} = robot
+    const [x, y] = position
+
+    const moveMapping: Record<Orientation, MarsCoordinates> = {
+        "N": [x, y + 1],
+        "E": [x + 1, y],
+        "S": [x, y - 1],
+        "W": [x - 1, y],
+    }
+
+    return moveMapping[orientation]
+}
+
+const moveForward = (robot: Robot): Robot => {
+    const {orientation, position} = robot
+    return {...robot, position: getNextPos(orientation, position)}
+
+}
+
+export const actionCommand = (cmd: string, robotState: Robot): Robot => {
+    const {orientation} = robotState
+
+    if (cmd === "L") return turnLeft(orientation)(robotState)
+    if (cmd === "R") return turnRight(orientation)(robotState)
+    if (cmd === "F") return moveForward(robotState)
+    return robotState
+}
+
+export const execute = (commands: string, robotState: Robot) => {
+    let res = robotState
+    const commandList = commands.split('')
+
+    commandList.forEach(cmd => {
+        res = actionCommand(cmd, robotState)
+    })
+    return res
+}
